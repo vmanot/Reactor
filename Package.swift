a5d3e288aab546669a4d3ac3aabdeb3b37f4c75f
Vatsal Manot <vatsal.manot@yahoo.com>
2019-12-12 21:40:34 +0530

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,35 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "Reduce",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
+    products: [
+        .library(name: "Reduce", targets: ["Reduce"])
+    ],
+    dependencies: [
+        .package(url: "git@github.com:vmanot/API.git", .branch("master")),
+        .package(url: "git@github.com:vmanot/CombineX.git", .branch("master")),
+        .package(url: "git@github.com:vmanot/SwiftUIX.git", .branch("master")),
+    ],
+    targets: [
+        .target(
+            name: "Reduce",
+            dependencies: [
+                "API",
+                "CombineX",
+                "SwiftUIX"
+            ],
+            path: "Sources"
+        ),
+    ],
+    swiftLanguageVersions: [
+        .version("5.1")
+    ]
+)
---
+++ Sources/Intramodular/Reduce.swift
@@ -0,0 +1,10 @@
+//
+// Copyright (c) Vatsal Manot
+//
+
+@_exported import API
+@_exported import Combine
+@_exported import CombineX
+@_exported import Swift
+@_exported import SwiftUI
+@_exported import SwiftUIX
