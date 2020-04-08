90a5461c540fef536cc8626ff53919f11e291e9e
Vatsal Manot <vatsal.manot@yahoo.com>
2020-04-08 19:49:46 +0530

    Update package

--- Sources/Intramodular/Router/Coordinator/ViewCoordinator.swift
+++ Sources/Intramodular/Router/Coordinator/ViewCoordinator.swift
@@ -98,15 +98,19 @@ open class BaseViewCoordinator<Route: ViewRoute>: OpaqueBaseViewCoordinator, Vie
 
 @propertyWrapper
 public struct ReactorRouter<C: ViewCoordinator>: DynamicProperty {
-    @Environment(\.viewReactors) var environmentReactors
-    @EnvironmentObject public var _wrappedValue: AnyViewCoordinator<C.Route>
+    @Environment(\.viewReactors) var viewReactors
+    
+    @OptionalEnvironmentObject public var _wrappedValue0: AnyViewCoordinator<C.Route>?
+    @OptionalEnvironmentObject public var _wrappedValue1: C?
     
     public var wrappedValue: C {
-        _wrappedValue.environmentBuilder.transformEnvironment {
-            $0.viewReactors.insert(self.environmentReactors)
+        let result = _wrappedValue0?.base ?? _wrappedValue1!
+        
+        result.environmentBuilder.transformEnvironment {
+            $0.viewReactors.insert(self.viewReactors)
         }
         
-        return _wrappedValue.base as! C
+        return result as! C
     }
     
     public init() {
