fba946aec106fd5758dd6c56501c636fc12f54c2
Vatsal Manot <vatsal.manot@yahoo.com>
2020-05-07 22:47:27 +0530

    Update package

--- Sources/module.swift
+++ Sources/module.swift
@@ -3,9 +3,9 @@
 //
 
 @_exported import Combine
+@_exported import Coordinator
 @_exported import Merge
 @_exported import Swift
 @_exported import SwiftUI
 @_exported import SwiftUIX
 @_exported import Task
-
